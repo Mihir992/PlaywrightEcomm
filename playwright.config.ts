768e8859df8cd8655c3d73293e35816227f6dca0
Mihir992 <mpunatargithub@gmail.com>
2025-07-10 21:50:43 +0530

    changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  globalSetup: './tests/setup/globalSetup.ts',
+  globalSetup: './src/tests/setup/globalSetup.ts',
   testDir: './src/tests', 
   use: {
     storageState: 'tests/storage/storageState.json', // 🟢 Reuse session

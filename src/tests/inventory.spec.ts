a5eaada1498bc76659471bb67fb23d924e7337ab
Mihir992 <119054373+Mihir992@users.noreply.github.com>
2025-07-11 09:50:25 +0530

    Update inventory.spec.ts

--- src/tests/inventory.spec.ts
+++ src/tests/inventory.spec.ts
@@ -1,29 +1,28 @@
 import { test, expect } from "@playwright/test";
+import { performLogin } from "../utils/loginUtils";
 import { InventoryPage } from "../pages/InventoryPage";
 import { CartPage } from "../pages/CartPage";
 
-test("Fetch and interact with products on inventory page", async ({ page }) => {
-  // Step 1: Login using reusable login function
-  //await performLogin(page);
+test.use({ storageState: 'storageState.json' }); 
 
-  // Step 2: Instantiate InventoryPage
+test("Fetch and interact with products on inventory page", async ({ page }) => {
+   // ✅ Go to the inventory page explicitly
+  //await page.goto('https://www.saucedemo.com/inventory.html');
+
   const inventoryPage = new InventoryPage(page);
 
-  // Step 3: Fetch and print all products
+   // 🔹 Navigate to the page explicitly using method
+  await inventoryPage.navigateToPage();
+  await page.pause();
   const allProducts = await inventoryPage.getAllProducts();
-  //console.log("Product List:", allProducts);
-
-  // Step 4: Dynamically click a product by name (choose any from printed list)
+  console.log("Product List:", allProducts);
+  
   const productNameToClick = "Sauce Labs Backpack";
-   //Use this instead of just clicking the product
-  await inventoryPage.clickProductByName(productNameToClick);
-  // Log the product name before clicking
   console.log(`Clicking on product: ${productNameToClick}`);
+  await inventoryPage.clickProductByName(productNameToClick);
 
-  //await page.pause();
-  // Step 5:Now click the cart
   await inventoryPage.clickOnAddToCartButton();
 
-  //Step 6:Click on cart button and chain to CheckoutPage
-   const cartPage: CartPage = await inventoryPage.clickCartButton();
+  const cartPage: CartPage = await inventoryPage.clickCartButton();
+
 });
